106ee8cae92936eae24a446903dd7ae12997f7b3
AquaPi271 <tozimek@gmail.com>
2021-09-02 01:28:04 -0400

    Activate required extensions to create the Vulkan object.
    Also activate the GLFW extension so graphics can be displayed in a window.
    
    Safely destroy the Vulkan instance.
    
    Also adds sequence to query and display the available extensions.

--- TutorialSolution/main.cpp
+++ TutorialSolution/main.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
+#include <vector>
 
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
@@ -19,7 +20,8 @@ public:
 
 private:
 
-	GLFWwindow* window;
+	GLFWwindow* window;    // Window generated for Vulkan usage
+	VkInstance instance;   // Vulkan handle to instance
 
 	void initWindow() {
 		glfwInit();
@@ -30,7 +32,64 @@ private:
 		window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);  // 4th parameter picks monitor... interesting
 	}
 	void initVulkan() {
+		createInstance();
+	}
 
+	void createInstance() {
+		VkApplicationInfo appInfo{};    // Setup optional info to let Vulkan know how to optimize.
+		appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;  // Explicitly tell Vulkan the type of struct used.
+		appInfo.pApplicationName = "Hello Triangle";
+		appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+		appInfo.pEngineName = "No Engine";
+		appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+		appInfo.apiVersion = VK_API_VERSION_1_0;
+
+		VkInstanceCreateInfo createInfo{};  // Non-optional information which will specify global extensions and validation layers to use.
+		createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO; // Tell Vulkan type of struct, per usual...
+		createInfo.pApplicationInfo = &appInfo;
+
+		// Vulkan is platform agnostic... so extension is pointed to the engine for Windows windows (GLFW).
+		uint32_t glfwExtensionCount = 0;
+		const char** glfwExtensions;
+
+		displayAllExtensions();
+
+		glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+		createInfo.enabledExtensionCount = glfwExtensionCount;
+		createInfo.ppEnabledExtensionNames = glfwExtensions;
+		createInfo.enabledLayerCount = 0;
+		// Other 'createInfo' structure elements left blank and will be explained later.
+
+		VkResult result = vkCreateInstance(&createInfo, nullptr, &instance);
+		if (result != VK_SUCCESS) {
+			throw std::runtime_error("failed to create instance!");
+		}
+
+		// The above can be simplified to:
+		// if ( vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+		//     throw std::runtime_error("failed to create instance!");
+		// }
+
+		// General pattern of object creation with Vulkan:
+		// 1) Pointer to struct with creation info.
+		// 2) Pointer to custom allocator callbacks, always nullptr in this tutorial.
+		// 3) Pointer to the variable that stores the handle to the new object.
+
+		// VkResult will either be VK_SUCCESS on successful function call or an error code otherwise.
+	}
+
+	void displayAllExtensions() {
+		uint32_t extensionCount = 0;
+		vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+		std::vector<VkExtensionProperties> extensions(extensionCount);
+		vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+		
+		std::cout << "available extensions:\n";
+
+		for (const auto& extension : extensions) {
+			std::cout << "\t" << extension.extensionName << "\n";
+		}
 	}
 
 	void mainLoop() {
@@ -40,6 +99,8 @@ private:
 	}
 
 	void cleanup() {
+		vkDestroyInstance(instance, nullptr);
+
 		glfwDestroyWindow(window);
 
 		glfwTerminate();
